524c7f6f6137f16cfef88a463515f346e69264a9
Deven Desai <desaidragon@gmail.com>
2016-07-05 15:19:15 -0400

    first

---
+++ Nothing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nothing
+//
+//  Created by Desai Chia on 7/5/16.
+//  Copyright © 2016 Deven Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nothing/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Nothing
+//
+//  Created by Desai Chia on 7/5/16.
+//  Copyright © 2016 Deven Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *myTextField;
+
+@end
+
